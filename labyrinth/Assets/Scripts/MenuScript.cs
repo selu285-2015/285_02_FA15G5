cbe0a51852bc4a6799a024343e829621619c59e2
Cammclean <cameron.mclean@selu.edu>
2015-09-27 20:57:15 -0500

    Start menu fix and lighting bake
    Reworked the menu script to play the level--Application.loadlevel was
    being called on start which I guess freaked Unity out.  I also baked the
    lighting on the maze so if there are any changes to it we'll need to
    remember to rebake it to apply the lighting changes.  More folder
    organization too.

--- labyrinth/Assets/Scripts/MenuScript.cs
+++ labyrinth/Assets/Scripts/MenuScript.cs
@@ -3,14 +3,34 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class MenuScript : MonoBehaviour {
+public class MenuScript : MonoBehaviour
+{
 
 	// Use this for initialization
 	public void Start () {
         Application.LoadLevel("main");
+    public Button start;
+    public Button settings;
+    public Button exit;
+
+    public void Start ()
+    {
+        start = start.GetComponent<Button>();
+        settings = settings.GetComponent<Button>();
+        exit = exit.GetComponent<Button>();
+    }
+    
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
     }
 	
 	// Update is called once per frame
 	public void Exit () {
+    
+    public void ExitGame()
+    {
         Application.Quit();
     }
+}
 }
