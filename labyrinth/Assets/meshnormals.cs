49c979a5916dad3350e506afe976d9479c007708
Salvetori <matt.salvetti@live.com>
2015-09-28 19:44:41 -0500

    Fixed the lighting
    it was the auto-lightmap all along

---
+++ labyrinth/Assets/meshnormals.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meshnormals : MonoBehaviour {
+	void Start() {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.RecalculateNormals();
+	}
+}
