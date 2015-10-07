ce7aa4a05cd2d387b6b1fd5138c9381e594a7ef6
Chad Bender <cbende2@lsu.edu>
2015-10-07 17:48:41 -0500

    GUI Timer
    Added a GUI Timer in Scripts.
    This timer currently starts when the "main" scene is loaded. ***Unable
    to test what happens after death from minotaur. Not sure if timer
    restarts or continues***

---
+++ labyrinth/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	public int time = 0;
+	public int seconds = 0;
+	public int minutes = 0;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine ("PlayTime");
+	}
+	
+	private IEnumerator PlayTime()
+	{
+		while (true) 
+		{
+			yield return new WaitForSeconds(1);
+			time += 1;
+			seconds = (time % 60);
+			minutes = (time / 60) % 60;
+		}
+	}
+	
+	void OnGUI ()
+	{
+		GUI.contentColor = Color.red;
+		GUI.Label (new Rect (900, 4, 400, 50),"<size=30>"+ minutes.ToString() + ":" + seconds.ToString()+ "</size>");
+	}
+}
