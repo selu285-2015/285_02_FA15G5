da7d533f7756c735e290185eb132c87bfa66fdb8
w0475768 <jason.mcneill@selu.edu>
2015-10-26 21:52:58 -0500

    Consistent fonts, Jacob's menu
    Alright, so Jacob's menu is visually good to present. As of right now,
    the choices for resolution is the only weird thing, still with numbers
    "x"d out. I matched all of the fonts except for the end credits with
    Jacob's font at the beginning of the game.

--- labyrinth/Assets/Scripts/MenuScript.cs
+++ labyrinth/Assets/Scripts/MenuScript.cs
@@ -8,16 +8,16 @@ public class MenuScript : MonoBehaviour
     public Button start;
     public Button settings;
     public Button exit;
-    public Button FullScreen;
-    //public bool SettingMenu;
+    public Toggle FullScreen;
     public Canvas SettingMenu;
+    public Dropdown ResDropDown;
 
     public void Start ()
     {
         start = start.GetComponent<Button>();
         settings = settings.GetComponent<Button>();
         exit = exit.GetComponent<Button>();
-        FullScreen = FullScreen.GetComponent<Button>();
+        FullScreen = FullScreen.GetComponent<Toggle>();
         SettingMenu.enabled = false;
 
     }
@@ -44,13 +44,56 @@ public class MenuScript : MonoBehaviour
     }
 
     public void SetFullScreen() {
-        // Screen.SetResolution(600, 800, true, 60);
         if (Screen.fullScreen == true)
            Screen.fullScreen = false;
         else
            Screen.fullScreen = true;
     }
-
+    public void SetResolution()
+    {
+        switch (ResDropDown.value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1680, 1050, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(1600, 1024, Screen.fullScreen);
+                break;
+            case 3:
+                Screen.SetResolution(1600, 900, Screen.fullScreen);
+                break;
+            case 4:
+                Screen.SetResolution(1400, 900, Screen.fullScreen);
+                break;
+            case 5:
+                Screen.SetResolution(1366, 768, Screen.fullScreen);
+                break;
+            case 6:
+                Screen.SetResolution(1360, 768, Screen.fullScreen);
+                break;
+            case 7:
+                Screen.SetResolution(1280, 1024, Screen.fullScreen);
+                break;
+            case 8:
+                Screen.SetResolution(1280, 960, Screen.fullScreen);
+                break;
+            case 9:
+                Screen.SetResolution(1280, 800, Screen.fullScreen);
+                break;
+            case 10:
+                Screen.SetResolution(1280, 768, Screen.fullScreen);
+                break;
+            case 11:
+                Screen.SetResolution(1152, 864, Screen.fullScreen);
+                break;
+            case 12:
+                Screen.SetResolution(1024, 768, Screen.fullScreen);
+                break;
+        }
+    }
     public void SetVolume()
     {
 
